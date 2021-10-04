7a80baeeaf0675ce568afcd1b830c8486ebe792b
Gabriel Henrique <gabrielhcm01@gmail.com>
2021-10-04 12:46:16 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,14 @@
+import Main from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Main',
+  component: Main
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+
+Basic.args = {
+  title: 'React Avançado',
+  description: 'TypeScript, ReactJS, NextJS e Styled Components'
+}
