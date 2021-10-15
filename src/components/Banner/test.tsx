dbecaf8ce3956a0ee838af1c09b370c1a9c6acb6
Gabriel Henrique <gabrielhcm01@gmail.com>
2021-10-14 21:39:05 -0300

    Adding base files for banner

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    // verificar se o { title, subtlte, img } existe
+    renderWithTheme(<Banner {...props} />)
+    //expect(screen.logTestingPlaygroundURL(container))
+    expect(screen.getByRole('heading', { name: /defy death/i }))
+      .toBeInTheDocument
+    expect(
+      screen.getByRole('heading', { name: /play the new crashlands season/i })
+    ).toBeInTheDocument
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument
+  })
+})
